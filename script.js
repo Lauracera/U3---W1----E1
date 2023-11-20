11ede91bbf5e6acef5bc1ec5b22d97fea5e00a1a
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-20 16:57:09 +0100

    quasi fatto

--- script.js
+++ script.js
@@ -8,37 +8,43 @@ var getRandomNumber = function () {
     return Math.floor(Math.random() * (100 - 1) + 1);
 };
 var checkWinner = function () {
-    //  let Player1: number = getRandomNumber(); 
-    //  console.log("Giocatore 1:", Player1);
-    //  let Player2: number = getRandomNumber(); 
-    //  console.log("Giocatore 2:", Player2);
-    var num1 = document.getElementsByClassName("num1");
-    var num2 = document.getElementsByClassName("num2");
-    var Player1 = num1.value;
+    var Player1 = getRandomNumber();
     console.log("Giocatore 1:", Player1);
-    var Player2 = num2.value;
+    var Player2 = getRandomNumber();
     console.log("Giocatore 2:", Player2);
+    // let num1: any = document.getElementsByClassName("num1");
+    // let num2: any = document.getElementsByClassName("num2");
+    //  let Player1: number = num1.value; 
+    //  console.log("Giocatore 1:", Player1);
+    //  let Player2: number = num2.value; 
+    //  console.log("Giocatore 2:", Player2);
     var randomDiv = document.getElementById("randomDiv");
-    var randomNumber = document.getElementById("randomNumber");
-    randomNumber.value = 80;
-    console.log(randomNumber);
+    var randomNumber1 = document.getElementById("randomNumber");
+    randomNumber1.innerHTML = "<h2>".concat(randomNumber, "</h2>");
+    randomNumber1.classList.add("center");
+    console.log(randomNumber1);
     var text = document.getElementById("text");
+    text.classList.add("text-center");
+    randomDiv.appendChild(randomNumber1);
     randomDiv.appendChild(text);
-    randomDiv.appendChild(randomNumber);
     console.log(randomDiv);
     var diffPlayer1 = Math.abs(Player1 - randomNumber);
     var diffPlayer2 = Math.abs(Player2 - randomNumber);
     if (Player1 === randomNumber) {
         console.log("il giocatore 1 ha indovinato");
+        text.innerText = "il giocatore 1 ha indovinato";
     }
     else if (Player2 === randomNumber) {
         console.log("Il giocatore 2 ha indovinato");
+        text.innerText = "Il giocatore 2 ha indovinato";
     }
     else if (diffPlayer1 < diffPlayer2) {
         console.log("Non avete azzeccato, ma il giocatore 1 si è avvicinato di più");
+        text.innerText = "Non avete azzeccato, ma il giocatore 1 si è avvicinato di più";
     }
     else if (diffPlayer2 < diffPlayer1) {
         console.log("Non avete azzeccato, ma il giocatore 2 si è avvicinato di più");
+        text.innerText = "Non avete azzeccato, ma il giocatore 2 si è avvicinato di più";
     }
 };
 checkWinner();
--- script.ts
+++ script.ts
@@ -20,35 +20,36 @@ const checkWinner = function () {
 
 
 
-//  let Player1: number = getRandomNumber(); 
-//  console.log("Giocatore 1:", Player1);
-//  let Player2: number = getRandomNumber(); 
-//  console.log("Giocatore 2:", Player2);
+ let Player1: number = getRandomNumber(); 
+ console.log("Giocatore 1:", Player1);
+ let Player2: number = getRandomNumber(); 
+ console.log("Giocatore 2:", Player2);
 
  
-let num1: any = document.getElementsByClassName("num1");
-let num2: any = document.getElementsByClassName("num2");
+// let num1: any = document.getElementsByClassName("num1");
+// let num2: any = document.getElementsByClassName("num2");
 
- let Player1: number = num1.value; 
- console.log("Giocatore 1:", Player1);
- let Player2: number = num2.value; 
- console.log("Giocatore 2:", Player2);
+//  let Player1: number = num1.value; 
+//  console.log("Giocatore 1:", Player1);
+//  let Player2: number = num2.value; 
+//  console.log("Giocatore 2:", Player2);
 
 
 const randomDiv = document.getElementById("randomDiv")
-randomDiv.innerHtml = 
+
+ const randomNumber1: any=
+  document.getElementById("randomNumber")
+ randomNumber1.innerHTML =   `<h2>${randomNumber}</h2>`
+ randomNumber1.classList.add("center")
+ console.log(randomNumber1)
+
+const text = document.getElementById("text")
+text.classList.add("text-center")
 
 
-//  const randomNumber: any=
-//   document.getElementById("randomNumber")
-//  randomNumber.inner = "80" 
-//  console.log(randomNumber)
 
-// const text = document.getElementById("text")
-
-
+randomDiv.appendChild(randomNumber1)
 randomDiv.appendChild(text)
-randomDiv.appendChild(randomNumber)
 console.log(randomDiv)
 
 
@@ -59,12 +60,16 @@ const diffPlayer2 : number = Math.abs(Player2 - randomNumber)
 
  if(Player1 === randomNumber) {
     console.log("il giocatore 1 ha indovinato")
+    text.innerText = "il giocatore 1 ha indovinato"
  } else if (Player2 === randomNumber) {
     console.log("Il giocatore 2 ha indovinato")
+    text.innerText = "Il giocatore 2 ha indovinato"
  } else if (diffPlayer1 < diffPlayer2) {
     console.log("Non avete azzeccato, ma il giocatore 1 si è avvicinato di più")
+    text.innerText = "Non avete azzeccato, ma il giocatore 1 si è avvicinato di più"
  } else if (diffPlayer2 < diffPlayer1) {
     console.log("Non avete azzeccato, ma il giocatore 2 si è avvicinato di più")
+    text.innerText = "Non avete azzeccato, ma il giocatore 2 si è avvicinato di più"
  }
 };
 
