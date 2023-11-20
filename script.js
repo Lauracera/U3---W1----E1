97f82024fcf6fae88923e3c7a228f74b9db4fd49
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-20 16:06:10 +0100

    random number fatto

--- script.js
+++ script.js
@@ -7,15 +7,41 @@ var form = document.getElementsByClassName(".formPlayer");
 // form.onsubmit(); { 
 //     if (Player1.value === "" && Player2.value === "") {
 //         alert("Inserisci un numero")
-//     } 
+// //     } 
+// // }
+//     const getRandomNumber = function (min: number, max : number): number {
+//     return Math.floor(Math.random() * (max - min)+ min) 
 // }
-var getRandomNumber = function (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-};
-var randomDiv = document.getElementById("randomDiv");
-var randomNumber = document.getElementById("randomNumber");
-randomNumber.value = Math.abs(randomNumber);
+// const randomDiv = document.getElementById("randomDiv")
+//  const randomNumber: any= document.getElementById("randomNumber")
+//  randomNumber.innerHTML = 
+//  console.log(randomNumber)
+// randomDiv.appendChild(randomNumber)
+// console.log(randomDiv)
+//  console.log(getRandomNumber(1, 100))
+var randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
 console.log(randomNumber);
-randomDiv.appendChild(randomNumber);
-console.log(randomDiv);
-console.log(getRandomNumber(1, 100));
+var getRandomNumber = function () {
+    return Math.floor(Math.random() * (100 - 1) + 1);
+};
+var checkWinner = function () {
+    var Player1 = getRandomNumber();
+    console.log("Giocatore 1:", Player1);
+    var Player2 = getRandomNumber();
+    console.log("Giocatore 2:", Player2);
+    var diffPlayer1 = Math.abs(Player1 - randomNumber);
+    var diffPlayer2 = Math.abs(Player2 - randomNumber);
+    if (Player1 === randomNumber) {
+        console.log("il giocatore 1 ha indovinato");
+    }
+    else if (Player2 === randomNumber) {
+        console.log("Il giocatore 2 ha indovinato");
+    }
+    else if (diffPlayer1 < diffPlayer2) {
+        console.log("Non avete azzeccato, ma il giocatore 1 si è avvicinato di più");
+    }
+    else if (diffPlayer2 < diffPlayer1) {
+        console.log("Non avete azzeccato, ma il giocatore 2 si è avvicinato di più");
+    }
+};
+checkWinner();
--- script.ts
+++ script.ts
@@ -7,31 +7,38 @@ let Player2: any = document.getElementsByClassName(".num2");
 let btn: any = document.getElementsByClassName(".btn");
 let form: any = document.getElementsByClassName(".formPlayer");
 
-// form.onsubmit(); { 
-//     if (Player1.value === "" && Player2.value === "") {
-//         alert("Inserisci un numero")
 
-//     } 
+ const randomNumber: number = 
+ Math.floor(Math.random() * (100 - 1) + 1);
+console.log(randomNumber);
 
-// }
+const getRandomNumber = function (): number
+{
+ return Math.floor(Math.random() * (100 - 1) + 1);
+};
 
-
-
-    const getRandomNumber = function (min: number, max : number): number {
-    return Math.floor(Math.random() * (max - min)+ min) 
-}
-
-const randomDiv = document.getElementById("randomDiv")
-
- const randomNumber: any= document.getElementById("randomNumber")
- randomNumber.value = Math.abs()
- console.log(randomNumber)
-
-randomDiv.appendChild(randomNumber)
-console.log(randomDiv)
-
- console.log(getRandomNumber(1, 100))
+const checkWinner = function () {
 
 
 
+ const Player1: number = getRandomNumber(); 
+ console.log("Giocatore 1:", Player1);
+ const Player2: number = getRandomNumber(); 
+ console.log("Giocatore 2:", Player2);
+
+const diffPlayer1 : number = Math.abs(Player1 - randomNumber)
+const diffPlayer2 : number = Math.abs(Player2 - randomNumber)
+
+ if(Player1 === randomNumber) {
+    console.log("il giocatore 1 ha indovinato")
+ } else if (Player2 === randomNumber) {
+    console.log("Il giocatore 2 ha indovinato")
+ } else if (diffPlayer1 < diffPlayer2) {
+    console.log("Non avete azzeccato, ma il giocatore 1 si è avvicinato di più")
+ } else if (diffPlayer2 < diffPlayer1) {
+    console.log("Non avete azzeccato, ma il giocatore 2 si è avvicinato di più")
+ }
+};
+
+checkWinner();
 
